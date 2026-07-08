15301ed6a48fe31879af23ccd01aa13cd0ffb7d7
Vince Churchill <104873546+ChurchillV@users.noreply.github.com>
2026-07-08 17:39:40 +0000

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.java
@@ -0,0 +1,29 @@
+class KthLargest {
+
+    PriorityQueue<Integer> queue;
+    int size;
+
+    public KthLargest(int k, int[] nums) {
+        queue = new PriorityQueue<>();
+        
+        for(int num : nums) {
+            queue.offer(num);
+        }
+
+        size = k;
+
+        while(queue.size() > size) {
+            queue.poll();
+        }
+    }
+    
+    public int add(int val) {
+        queue.offer(val);
+
+        while(queue.size() > size) {
+            queue.poll();
+        }
+
+        return queue.peek();
+    }
+}
