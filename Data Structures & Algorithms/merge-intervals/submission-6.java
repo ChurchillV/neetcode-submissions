cad5656e04e886fdf0e5dc531077bc59f1708d51
Vince Churchill <104873546+ChurchillV@users.noreply.github.com>
2026-08-10 15:39:35 +0000

    Add: merge-intervals - submission-6

---
+++ Data Structures & Algorithms/merge-intervals/submission-6.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>((a,b) -> Integer.compare(a[0], b[0]));
+
+        for(int[] interval : intervals) {
+            queue.offer(interval);
+        }
+
+        List<int[]> result = new ArrayList();
+        int SIZE = intervals.length;
+        int[] prev = queue.poll();
+
+        for(int i = 1; i < SIZE; i++) {
+            int[] interval = queue.poll();
+
+            if(interval[0] > prev[1]) {
+                result.add(prev);
+                prev = interval;
+            }
+
+            else {
+                prev[0] = Math.min(prev[0], interval[0]);
+                prev[1] = Math.max(prev[1], interval[1]);
+            }
+        }
+
+        result.add(prev);
+        return result.stream()  
+                    .toArray(int[][]::new);
+
+    }
+}
