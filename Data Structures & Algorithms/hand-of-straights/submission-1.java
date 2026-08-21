ef5eaaf5b8f9c510ee9bceb9c0e258aad547f2d4
Vince Churchill <104873546+ChurchillV@users.noreply.github.com>
2026-08-21 17:41:35 +0000

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap();
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for(int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        for(int key : map.keySet()) {
+            heap.offer(key);
+        }
+
+        while(!heap.isEmpty()) {
+            int min = heap.peek();
+
+            for(int i = min; i < min + groupSize; i++) {
+                if(!map.containsKey(i)) {
+                    return false;
+                }
+
+                map.put(i, map.get(i) - 1);
+
+                if(map.get(i) == 0) {
+                    if(i != heap.peek()) {
+                        return false;
+                    }
+
+                    heap.poll();
+                }
+
+            }
+        }
+
+        return true;
+    }
+}
